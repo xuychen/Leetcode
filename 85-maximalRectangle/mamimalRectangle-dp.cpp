dd8586a00e44eb7d24f1103871e4fea217d97fbf
xuychen <xuychen@ucdavis.edu>
2018-07-21 16:40:41 -0700

    reference for Leetcode 85

---
+++ 85-maximalRectangle/mamimalRectangle-dp.cpp
@@ -0,0 +1,54 @@
+// this solution is from Leetcode, which is not my work
+// since I cannot find a way to improve it, I just leave it as it is 
+
+// add libraries to get rid of compiling errors
+#include <vector>
+#include <math>
+
+// body
+class Solution {
+    public:
+    int maximalRectangle(vector< vector<char> > &matrix) {
+        if (matrix.empty()) return 0;
+        const int m = matrix.size();
+        const int n = matrix[0].size();
+        int left[n], right[n], height[n];
+        fill_n(left, n, 0);
+        fill_n(right, n, n);
+        fill_n(height, n, 0);
+        int maxA = 0;
+        for (int i = 0; i < m; i++) {
+            int cur_left = 0, cur_right = n;
+            for (int j = 0; j < n;
+                 j++) {  // compute height (can do this from either side)
+                if (matrix[i][j] == '1')
+                    height[j]++;
+                else
+                    height[j] = 0;
+            }
+            for (int j = 0; j < n; j++) {  // compute left (from left to right)
+                if (matrix[i][j] == '1')
+                    left[j] = max(left[j], cur_left);
+                else {
+                    left[j] = 0;
+                    cur_left = j + 1;
+                }
+            }
+            // compute right (from right to left)
+            for (int j = n - 1; j >= 0; j--) {
+                cout << right[j] << cur_right << endl;
+                if (matrix[i][j] == '1')
+                    right[j] = min(right[j], cur_right);
+                else {
+                    right[j] = n;
+                    cur_right = j;
+                }
+            }
+
+            // compute the area of rectangle (can do this from either side)
+            for (int j = 0; j < n; j++)
+                maxA = max(maxA, (right[j] - left[j]) * height[j]);
+        }
+        return maxA;
+    }
+};
