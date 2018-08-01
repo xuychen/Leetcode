bfe67b69711caa74be0ec66e21be434617fc3b99
xuychen <xuychen@ucdavis.edu>
2018-08-01 15:59:35 -0700

    shorten the code

--- 31-40/34-firstLastPositionOfElement/firstLastPositionOfElement.py
+++ 31-40/34-firstLastPositionOfElement/firstLastPositionOfElement.py
@@ -6,10 +6,7 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        start = end = -1
-        length = len(nums)
-        left, right = 0, length
-        index = -1
+        left, right = 0, len(nums)
         
         # find target first
         while left < right:
@@ -19,17 +16,10 @@ class Solution(object):
             elif nums[mid] < target:
                 left = mid + 1
             else:
-                index = mid
-                break
+                start, end = self.searchBounds(nums, target, left, mid), self.searchBounds(nums, target+1, mid, right) - 1
+                return [start, end]
                 
-        if index != -1:
-            start, end = self.searchBounds(nums, target, left, index), self.searchBounds(nums, target+1, index, right)
-            if nums[start] != target:
-                start += 1
-            if end == length or nums[end] != target:
-                end -= 1
-                
-        return [start, end]
+        return [-1, -1]
     
     def searchBounds(self, nums, target, left, right):
         while left < right:
