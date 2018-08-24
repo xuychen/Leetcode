63732429d3d2bb3bb19f8f8dc6e754e330ab760a
xuychen <xuychen@ucdavis.edu>
2018-08-23 21:27:45 -0700

    bug fix

--- arrangeQuestions.sh
+++ arrangeQuestions.sh
@@ -3,10 +3,9 @@
 cd $1
 except=' miscellaneous arrangeQuestions.sh '
 
-for file in *
-do
+for file in *; do
     if [[ $file == .* || $except =~ (.* $file .*) ]]; then
-        echo File \"$file\" is skipped
+        echo \"$file\" is skipped
         continue
     fi
 
@@ -25,44 +24,50 @@ do
         echo FILE \"$file\" is putting in Directory \"$DIR\"
         echo Please give it a question number at the beginning of Directory
     else
-        if [[ $file == *-[^0-9]* ]]; then
-            INDEX=${file%%-*}
-            if [[ ! $INDEX =~ [0-9]+ ]]; then
-                echo Directory \"$file\" is not a valid Directory
-                continue
-            fi
+        INDEX=$((${file%%-*}-1))
+        if [[ ! $INDEX =~ [0-9]+ ]]; then
+            echo Directory \"$file\" is not a valid Directory
+            continue
+        fi
 
+        START100=$((INDEX/100*100+1))
+        DIR100=$START100-$((START100+99))
+
+        if [[ $file == *-[^0-9]* ]]; then
             START=$((INDEX/10*10+1))
             DIR=$START-$((START+9))
 
-            if [ ! -d $DIR ]; then
-                echo creating Directory \"$DIR\"
-                mkdir $DIR
-            fi
+            if [ -d "$DIR100/$DIR" ]; then
+                mv $file $DIR100/$DIR
+                echo Directory \"$file\" is putting in Directory \"$DIR100/$DIR\"
+            else
+                if [ ! -d $DIR ]; then
+                    echo creating Directory \"$DIR\"
+                    mkdir $DIR
+                fi
 
-            mv $file $DIR
-            echo sub-Directory \"$file\" is putting in Directory \"$DIR\"
+                mv $file $DIR
+                echo sub-Directory \"$file\" is putting in Directory \"$DIR\"
             
-            file=$DIR
+                file=$DIR
+            fi
         fi
 
-        if [[ $file =~ .*[1-9]0$ ]]; then
-            INDEX=${file%%-*}
-            if [[ ! $INDEX =~ [0-9]+ ]]; then
-                echo Directory \"$file\" is not a valid Directory
-                continue
+        if [[ $file =~ .*[0-9]0$ && $file != $DIR100 ]]; then
+            if [ ! -d $DIR100 ]; then
+                echo creating Directory \"$DIR100\"
+                mkdir $DIR100
+            fi
+    
+            if [ -d "$DIR100/$file" ]; then
+                echo exist a Directory with the same name under Directory \"$DIR100\"
+                mv $file/* $DIR100/$file
+                rmdir $file
+            else
+                mv $file $DIR100
             fi
 
-            START=$((INDEX/100*100+1))
-            DIR=$START-$((START+99))
-
-            if [ ! -d $DIR ]; then
-                echo creating Directory \"$DIR\"
-                mkdir $DIR
-            fi
-
-            mv $file $DIR
-            echo sub-Directory \"$file\" is putting in Directory \"$DIR\"
+            echo sub-Directory \"$file\" is putting in Directory \"$DIR100\"
         fi
     fi
 done
