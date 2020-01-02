82827fb82d835374855b04d6ba8d9fb5c7120d14
xuychen <xuychen@ucdavis.edu>
2020-01-01 17:46:23 -0800

    add new solution Jan 1 2020

--- 1-100/1-10/4-medianOfTwoSortedArray/medianOfTwoSortedArray.cpp
+++ 1-100/1-10/4-medianOfTwoSortedArray/medianOfTwoSortedArray.cpp
@@ -6,10 +6,11 @@ using namespace std;
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int N1 = nums1.size();
-        int N2 = nums2.size();
-        if (N1 < N2)
-            return findMedianSortedArrays(nums2, nums1);  // Make sure A2 is the shorter one.
+        int N1 = nums1.size(), N2 = nums2.size();
+        if (N1 < N2) {
+            nums1.swap(nums2);  // Make sure A2 is the shorter one.
+            swap(N1, N2);
+        }
 
         int lo = 0, hi = N2 * 2;
         while (lo <= hi) {
@@ -28,6 +29,36 @@ public:
             else
                 return (max(L1, L2) + min(R1, R2)) / 2;  // Otherwise, that's the right cut.
         }
+        return -1;
+    }
+
+    double findMedianSortedArrays2(vector<int>& nums1, vector<int>& nums2) {
+        size_t length1 = nums1.size(), length2 = nums2.size();
+
+        if (length1 > length2) {
+            nums1.swap(nums2);
+            swap(length1, length2);
+        }
+
+        int low = 0, high = length1;
+        while (low <= high) {
+            int i = (low + high) / 2;
+            int j = (length1 + length2 + 1) / 2 - i;
+
+            if (i > 0 && nums1[i-1] > nums2[j])
+                high = i - 1;
+            else if (i < length1 && nums2[j-1] > nums1[i])
+                low = i + 1;
+            else {
+                int l1 = i == 0 ? INT_MIN : nums1[i-1];
+                int l2 = j == 0 ? INT_MIN : nums2[j-1];
+                int r1 = i == length1 ? INT_MAX : nums1[i];
+                int r2 = j == length2 ? INT_MAX : nums2[j];
+
+                return (length1 + length2) & 1 ? max(l1, l2) : (max(l1, l2) + min(r1, r2)) / 2.0;
+            }
+        }
+
         return -1;
     }
 };
