3c988d3378f48641f0bcb12d1acfda6064431858
xuychen <xuychen@ucdavis.edu>
2020-03-05 01:48:23 -0800

    add new functionality

--- arrangeQuestions.sh
+++ arrangeQuestions.sh
@@ -4,18 +4,22 @@ cd $1
 except=' miscellaneous arrangeQuestions.sh createFile.sh '
 
 for file in *; do
+    # skippingn files in variable except
     if [[ $file == .* || $except =~ (.* $file .*) ]]; then
         echo \"$file\" is skipped
         continue
     fi
 
     if [ -f $file ]; then
-        DIR=${file%%.*}
-        SHORT=${DIR%%-*}
+        # if this is a file
+        DIR=${file%%.*} # filename without extension
+        SHORT=${DIR%%-*} # question number
 
         if [[ -d $SHORT || -e $SHORT.* ]]; then
+            # if there is a matched directory
             DIR=$SHORT
         elif [ ! -d $DIR ]; then
+            # create one if does not exist
             echo creating Directory \"$DIR\"
             mkdir $DIR
         fi
@@ -24,23 +28,41 @@ for file in *; do
         echo FILE \"$file\" is putting in Directory \"$DIR\"
         echo Please give it a question number at the beginning of Directory
     else
-        INDEX=$((${file%%-*}-1))
+        # if this is a directory
+        INDEX=$((${file%%-*}-1)) # questions 1-10 have index 0-9
         if [[ ! $INDEX =~ [0-9]+ ]]; then
             echo Directory \"$file\" is not a valid Directory
             continue
         fi
 
-        START100=$((INDEX/100*100+1))
-        DIR100=$START100-$((START100+99))
+        START100=$((INDEX/100*100+1)) # directory start_index
+        DIR100=$START100-$((START100+99)) # directory end_index
 
         if [[ $file == *-[^0-9]* ]]; then
+            # if it is a base directory, e.g. 35-findInsertPosition
             START=$((INDEX/10*10+1))
             DIR=$START-$((START+9))
+            PATH10=$DIR100/$DIR
+            PATH1=$DIR100/$DIR/$file
 
-            if [ -d "$DIR100/$DIR" ]; then
-                mv $file $DIR100/$DIR
-                echo Directory \"$file\" is putting in Directory \"$DIR100/$DIR\"
+            if [ -d $PATH1 ]; then
+                # if base directory exists in main catalogs
+                for SUB_FILE in $file/*; do
+                    if [ -f $PATH1/${SUB_FILE##*/} ]; then
+                        # if file exists
+                        echo File \"${SUB_FILE##*/}\" exists in Directory \"$PATH1\"
+                    else
+                        # else
+                        mv $SUB_FILE $PATH1
+                        echo File \"${SUB_FILE##*/}\" is putting in Directory \"$PATH1\"
+                    fi
+                done
+            elif [ -d $PATH10 ]; then
+                # if base directory does not exist in main catalogs
+                mv $file $PATH10
+                echo Directory \"$file\" is putting in Directory \"$PATH10\"
             else
+                # if the directory to place base directory does not exist yet
                 if [ ! -d $DIR ]; then
                     echo creating Directory \"$DIR\"
                     mkdir $DIR
@@ -48,7 +70,7 @@ for file in *; do
 
                 mv $file $DIR
                 echo sub-Directory \"$file\" is putting in Directory \"$DIR\"
-            
+
                 file=$DIR
             fi
         fi
@@ -58,7 +80,7 @@ for file in *; do
                 echo creating Directory \"$DIR100\"
                 mkdir $DIR100
             fi
-    
+
             if [ -d "$DIR100/$file" ]; then
                 echo exist a Directory with the same name under Directory \"$DIR100\"
                 mv $file/* $DIR100/$file
