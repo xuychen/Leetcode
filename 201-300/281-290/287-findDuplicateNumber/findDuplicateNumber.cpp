14d1143d3fb92c3f065da6402af9e87e0f1274d8
xuychen <xuychen@ucdavis.edu>
2020-06-20 23:15:19 -0700

    add solution

--- 201-300/281-290/287-findDuplicateNumber/findDuplicateNumber.cpp
+++ 201-300/281-290/287-findDuplicateNumber/findDuplicateNumber.cpp
@@ -18,4 +18,16 @@ public:
         }
         return -1;
     }
+
+    int findDuplicate2(vector<int>& nums) {
+        int l = 1, r = nums.size() - 1;
+        while (l < r) {
+            int m = (l + r) / 2, n = 0;
+            for (int num : nums)
+                n += num <= m;
+
+            n <= m ? l = m + 1 : r = m;
+        }
+        return l;
+    }
 };
