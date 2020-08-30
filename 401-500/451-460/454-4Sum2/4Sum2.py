2105855f5f12e152dbd49f503a2962b09492df5c
xuychen <xuychen@ucdavis.edu>
2020-08-29 21:52:42 -0700

    little revision

--- 401-500/451-460/454-4Sum2/4Sum2.py
+++ 401-500/451-460/454-4Sum2/4Sum2.py
@@ -1,3 +1,6 @@
+import collections
+import itertools
+
 class Solution(object):
     def fourSumCount(self, A, B, C, D, depth=4, summation=0):
         """
@@ -8,10 +11,10 @@ class Solution(object):
         :rtype: int
         """
 
-        summation1 = Counter(map(sum, itertools.product(A, B)))
-        summation2 = Counter(map(lambda x: -x[0]-x[1], itertools.product(C, D)))
+        summation1 = collections.Counter(map(sum, itertools.product(A, B)))
+        summation2 = collections.Counter(map(lambda x: -x[0]-x[1], itertools.product(C, D)))
         return sum([summation1[key] * summation2[key] for key in summation1])
 
     def fourSumCount2(self, A, B, C, D):
-        AB = collections.Counter(a+b for a in A for b in B)
-        return sum(AB[-c-d] for c in C for d in D)
+        counter = collections.Counter(a+b for a in A for b in B)
+        return sum(counter[-c-d] for c in C for d in D)
