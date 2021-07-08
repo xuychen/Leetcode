709d2c35845444983866a8055a817b6dceb9c72b
xuychen <xuychen@ucdavis.edu>
2021-07-08 17:36:46 +0800

    support 3 argvs create in directory directly

--- createFile.sh
+++ createFile.sh
@@ -16,18 +16,18 @@ if [[ ! $extension =~ (.* ${@: -1} .*) ]]; then
 fi
 
 INDEX=$(($1-1)) # questions 1-10 have index 0-9
-if [[ $# == 2 ]]; then
-    START100=$(($INDEX/100*100+1)) # directory start_index
-    START10=$(($INDEX/10*10+1))
-    DIRECTORY=`find $START100-*/$START10-*/* -name "$1-*"`
+START100=$(($INDEX/100*100+1)) # directory start_index
+START10=$(($INDEX/10*10+1))
+DIRECTORY=`find $START100-*/$START10-*/* -name "$1-*"`
 
-    if [ -z $DIRECTORY ]; then
+if [[ $# == 2 ]]; then
+    if [[ -z $DIRECTORY ]]; then
         echo Error: \"$1\" does not exist in directory
     else
         FILENAME=${DIRECTORY##*/$1-}
-        FILEPATH=`find $DIRECTORY/* -name "*.$2"`
+        FILEPATH=$DIRECTORY/$FILENAME.$2
 
-        if [ -z $FILEPATH ]; then
+        if [[ ! -e $FILEPATH ]]; then
             touch "$FILENAME.$2"
             mv "$FILENAME.$2" $DIRECTORY
             echo Question \"$1-$FILENAME.$2\" created in Directory \"$DIRECTORY\"
@@ -42,8 +42,8 @@ elif [[ $# == 3 ]]; then
         echo create File \"$FILENAME\"
     fi
 
-    DIRECTORY=$1-${2%%-*}
-    if [ ! -d $DIRECTORY ]; then
+    if [[ -z $DIRECTORY ]]; then
+        DIRECTORY=$1-$2
         mkdir $DIRECTORY
         echo creating Directory \"$DIRECTORY\"
     fi
