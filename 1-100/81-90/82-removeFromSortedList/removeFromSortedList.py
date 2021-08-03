7e8356d47c6f9a1f4372a415224c252914c95780
xuychen <xuychen@ucdavis.edu>
2021-08-03 19:33:55 +0800

    rename

--- 1-100/81-90/82-removeFromSortedList/removeFromSortedList.py
+++ 1-100/81-90/82-removeFromSortedList/removeFromSortedList.py
@@ -1,8 +1,8 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution(object):
     def deleteDuplicates(self, head):
@@ -10,12 +10,12 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        
+
         node = head.next if head else None
         start = head
         prev = None
         flag = False
-        
+
         while node:
             if node.val == start.val:
                 start.next = node.next
@@ -28,14 +28,41 @@ class Solution(object):
                         head = node
                 else:
                     prev = start
-                    
+
                 start = node
                 flag = False
             node = start.next
-        
+
         if flag:
             if prev:
                 prev.next = node
             else:
                 head = node
         return head
+
+    def deleteDuplicates2(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        dummy = ListNode(None, head)
+        start, end = dummy, head
+        count = 0
+
+        while end:
+            if start.next.val == end.val:
+                count += 1
+            elif count > 1:
+                start.next = end
+                count = 1
+            else:
+                start = start.next
+                count = 1
+
+            end = end.next
+
+        if count > 1:
+            start.next = end
+
+        return dummy.next
