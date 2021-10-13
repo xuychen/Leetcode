eb43755995490310a0dce58b816235d7fac30977
xuychen <xuychen@ucdavis.edu>
2021-10-14 00:07:01 +0800

    update directory

--- arrangeQuestions.sh
+++ arrangeQuestions.sh
@@ -8,7 +8,7 @@ if [ $# -gt 1 ]; then
     cd $1
 fi
 
-except=' miscellaneous interviews LCOF LCP LCCI arrangeQuestions.sh createFile.sh LICENSE README.md '
+except=' miscellaneous interviews LCCI LCOF LCOF2 LCP arrangeQuestions.sh createFile.sh LICENSE README.md '
 
 for file in *; do
     # skipping files in variable except
